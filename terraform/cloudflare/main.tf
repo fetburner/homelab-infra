6aed10792905435d023af347173f83054c3b0759
Masayuki Mizuno <mizuno@wantedly.com>
2026-07-19 07:36:12 +0900

    Import Cloudflare Zero Trust

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -152,3 +152,117 @@ resource "cloudflare_email_routing_rule" "forward_me" {
     value = "me@${cloudflare_zone.fetburner_dev.name}"
   }]
 }
+
+resource "cloudflare_zero_trust_access_application" "console_kagawa" {
+  account_id                 = cloudflare_zone.fetburner_dev.account.id
+  name                       = "console-kagawa"
+  type                       = "ssh"
+  domain                     = "console-kagawa.${cloudflare_zone.fetburner_dev.name}"
+  session_duration           = "24h"
+  app_launcher_visible       = true
+  auto_redirect_to_identity  = false
+  enable_binding_cookie      = false
+  http_only_cookie_attribute = true
+  options_preflight_bypass   = false
+  destinations = [{
+    type = "public"
+    uri  = "console-kagawa.${cloudflare_zone.fetburner_dev.name}"
+  }]
+  policies = [{
+    id         = "86b9512c-8921-4b36-8015-e1130853d10d"
+    precedence = 1
+  }]
+}
+
+resource "cloudflare_zero_trust_access_application" "epgstation_qa" {
+  account_id                 = cloudflare_zone.fetburner_dev.account.id
+  name                       = "epgstation-qa"
+  type                       = "self_hosted"
+  domain                     = "epgstation-qa.${cloudflare_zone.fetburner_dev.name}"
+  session_duration           = "24h"
+  app_launcher_visible       = true
+  auto_redirect_to_identity  = false
+  enable_binding_cookie      = false
+  http_only_cookie_attribute = true
+  options_preflight_bypass   = false
+  destinations = [{
+    type = "public"
+    uri  = "epgstation-qa.${cloudflare_zone.fetburner_dev.name}"
+  }]
+  policies = [{
+    id         = "36afc495-09a3-4052-a141-a47826c936e6"
+    precedence = 1
+  }]
+}
+
+resource "cloudflare_zero_trust_access_application" "epgstation" {
+  account_id                 = cloudflare_zone.fetburner_dev.account.id
+  name                       = "epgstation"
+  type                       = "self_hosted"
+  domain                     = "epgstation.${cloudflare_zone.fetburner_dev.name}"
+  session_duration           = "24h"
+  app_launcher_visible       = true
+  auto_redirect_to_identity  = false
+  enable_binding_cookie      = false
+  http_only_cookie_attribute = true
+  options_preflight_bypass   = false
+  destinations = [{
+    type = "public"
+    uri  = "epgstation.${cloudflare_zone.fetburner_dev.name}"
+  }]
+  policies = [{
+    id         = "422c3421-2093-4081-8e4b-0cac92a65488"
+    precedence = 1
+  }]
+}
+
+resource "cloudflare_zero_trust_access_application" "app_launcher" {
+  account_id                   = cloudflare_zone.fetburner_dev.account.id
+  name                         = "App Launcher"
+  type                         = "app_launcher"
+  domain                       = "fetburner.cloudflareaccess.com"
+  session_duration             = "24h"
+  auto_redirect_to_identity    = false
+  skip_app_launcher_login_page = false
+  landing_page_design = {
+    title = "Welcome!"
+  }
+}
+
+resource "cloudflare_zero_trust_access_policy" "fetburner" {
+  account_id       = cloudflare_zone.fetburner_dev.account.id
+  decision         = "allow"
+  name             = "fetburner"
+  session_duration = "24h"
+  include = [{
+    group = {
+      id = cloudflare_zero_trust_access_group.fetburner.id
+    }
+  }]
+}
+
+resource "cloudflare_zero_trust_access_group" "fetburner" {
+  account_id = cloudflare_zone.fetburner_dev.account.id
+  name       = "fetburner"
+  include = [{
+    login_method = { id = cloudflare_zero_trust_access_identity_provider.github.id }
+  }]
+  require = [{
+    email = { email = "masayuki3141592@gmail.com" }
+  }, {
+    geo = { country_code = "JP" }
+  }]
+}
+
+resource "cloudflare_zero_trust_access_identity_provider" "github" {
+  account_id = cloudflare_zone.fetburner_dev.account.id
+  name       = "GitHub"
+  type       = "github"
+  config     = { client_id = "Ov23ctMSHKb1EtXQSNvr" }
+  scim_config = {
+    enabled                  = false
+    identity_update_behavior = "no_action"
+    seat_deprovision         = false
+    user_deprovision         = false
+  }
+}
