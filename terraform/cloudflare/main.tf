324d1e274a02828e6c4c80732d408b3658dbbba6
Masayuki Mizuno <mizuno@wantedly.com>
2026-07-19 06:57:18 +0900

    Refactor Cloudflare settings

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -1,35 +1,41 @@
+locals {
+  account_id = "abdb81a14388bfb3b2ccfe00525c16df"
+  zone_id    = "cb1cd8b1395576b4d6d7c15154897f93"
+  domain     = "fetburner.dev"
+}
+
 resource "cloudflare_zone" "example" {
   account = {
-    id = "abdb81a14388bfb3b2ccfe00525c16df"
+    id = local.account_id
   }
-  name = "fetburner.dev"
+  name = local.domain
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "terraform_managed_resource_console_kagawa" {
-  account_id = "abdb81a14388bfb3b2ccfe00525c16df"
+  account_id = local.account_id
   config_src = "cloudflare"
   name       = "console-kagawa"
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "terraform_managed_resource_epgstation" {
-  account_id = "abdb81a14388bfb3b2ccfe00525c16df"
+  account_id = local.account_id
   config_src = "cloudflare"
   name       = "epgstation"
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "terraform_managed_resource_k3s" {
-  account_id = "abdb81a14388bfb3b2ccfe00525c16df"
+  account_id = local.account_id
   config_src = "cloudflare"
   name       = "k3s"
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "console_kagawa" {
-  account_id = "abdb81a14388bfb3b2ccfe00525c16df"
+  account_id = local.account_id
   tunnel_id  = "e795f54d-0905-4821-8959-4abd659f8d42"
   config = {
     ingress = [
       {
-        hostname       = "console-kagawa.fetburner.dev"
+        hostname       = "console-kagawa.${local.domain}"
         service        = "ssh://localhost:22"
         origin_request = {}
       },
@@ -41,12 +47,12 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "console_kagawa" {
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "epgstation" {
-  account_id = "abdb81a14388bfb3b2ccfe00525c16df"
+  account_id = local.account_id
   tunnel_id  = "6491c854-dd08-447f-9a31-a8066f197dec"
   config = {
     ingress = [
       {
-        hostname       = "epgstation.fetburner.dev"
+        hostname       = "epgstation.${local.domain}"
         service        = "http://nginx:80"
         origin_request = {}
       },
@@ -58,17 +64,17 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "epgstation" {
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "k3s" {
-  account_id = "abdb81a14388bfb3b2ccfe00525c16df"
+  account_id = local.account_id
   tunnel_id  = "87e9ac1d-2811-4771-938a-0085766308b2"
   config = {
     ingress = [
       {
-        hostname       = "epgstation-qa.fetburner.dev"
+        hostname       = "epgstation-qa.${local.domain}"
         service        = "http://nginx.epgstation.svc.cluster.local"
         origin_request = {}
       },
       {
-        hostname       = "rancher.fetburner.dev"
+        hostname       = "rancher.${local.domain}"
         service        = "http://rancher.cattle-system.svc.cluster.local"
         origin_request = {}
       },
@@ -81,59 +87,59 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "k3s" {
 
 resource "cloudflare_dns_record" "mx_route1" {
   content  = "route1.mx.cloudflare.net"
-  name     = "fetburner.dev"
+  name     = local.domain
   priority = 98
   proxied  = false
   tags     = []
   ttl      = 1
   type     = "MX"
-  zone_id  = "cb1cd8b1395576b4d6d7c15154897f93"
+  zone_id  = local.zone_id
   settings = {}
 }
 
 resource "cloudflare_dns_record" "mx_route2" {
   content  = "route2.mx.cloudflare.net"
-  name     = "fetburner.dev"
+  name     = local.domain
   priority = 76
   proxied  = false
   tags     = []
   ttl      = 1
   type     = "MX"
-  zone_id  = "cb1cd8b1395576b4d6d7c15154897f93"
+  zone_id  = local.zone_id
   settings = {}
 }
 
 resource "cloudflare_dns_record" "mx_route3" {
   content  = "route3.mx.cloudflare.net"
-  name     = "fetburner.dev"
+  name     = local.domain
   priority = 27
   proxied  = false
   tags     = []
   ttl      = 1
   type     = "MX"
-  zone_id  = "cb1cd8b1395576b4d6d7c15154897f93"
+  zone_id  = local.zone_id
   settings = {}
 }
 
 resource "cloudflare_dns_record" "txt_spf" {
   content  = "\"v=spf1 include:_spf.mx.cloudflare.net ~all\""
-  name     = "fetburner.dev"
+  name     = local.domain
   proxied  = false
   tags     = []
   ttl      = 1
   type     = "TXT"
-  zone_id  = "cb1cd8b1395576b4d6d7c15154897f93"
+  zone_id  = local.zone_id
   settings = {}
 }
 
 resource "cloudflare_dns_record" "txt_dkim" {
   content  = "\"v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiweykoi+o48IOGuP7GR3X0MOExCUDY/BCRHoWBnh3rChl7WhdyCxW3jgq1daEjPPqoi7sJvdg5hEQVsgVRQP4DcnQDVjGMbASQtrY4WmB1VebF+RPJB2ECPsEDTpeiI5ZyUAwJaVX7r6bznU67g7LvFq35yIo4sdlmtZGV+i0H4cpYH9+3JJ78k\" \"m4KXwaf9xUJCWF6nxeD+qG6Fyruw1Qlbds2r85U9dkNDVAS3gioCvELryh1TxKGiVTkg4wqHTyHfWsp7KD3WQHYJn0RyfJJu6YEmL77zonn7p2SRMvTMP3ZEXibnC9gz3nnhR6wcYL8Q7zXypKTMD58bTixDSJwIDAQAB\""
-  name     = "cf2024-1._domainkey.fetburner.dev"
+  name     = "cf2024-1._domainkey.${local.domain}"
   proxied  = false
   tags     = []
   ttl      = 1
   type     = "TXT"
-  zone_id  = "cb1cd8b1395576b4d6d7c15154897f93"
+  zone_id  = local.zone_id
   settings = {}
 }
 
@@ -141,7 +147,7 @@ resource "cloudflare_email_routing_rule" "forward_me" {
   enabled  = true
   name     = "Rule created at 2024-07-01T15:12:56.526Z"
   priority = 0
-  zone_id  = "cb1cd8b1395576b4d6d7c15154897f93"
+  zone_id  = local.zone_id
   actions = [{
     type  = "forward"
     value = ["masayuki3141592@gmail.com"]
@@ -149,6 +155,6 @@ resource "cloudflare_email_routing_rule" "forward_me" {
   matchers = [{
     field = "to"
     type  = "literal"
-    value = "me@fetburner.dev"
+    value = "me@${local.domain}"
   }]
 }
